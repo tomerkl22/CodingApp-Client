565d1d73ade08b50b908ffbe0d8ef8cb1ab821be
tomerkl22 <94317058+tomerkl22@users.noreply.github.com>
2024-07-25 02:25:04 +0300

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'http://localhost:8080/';
+const API_BASE_URL = 'https://codingapp-server-production.up.railway.app';
 
 const apiClient= axios.create(
     {
